866c72d05a206fba30a661ea90a23ec5d5460a66
Dom-Marra <dominicmarra321@gmail.com>
2021-11-06 17:47:30 -0700

    Wait for image to load and added srcset and sizes to image

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,9 +9,9 @@ import useOnScreen from "../hooks/useOnScreen";
 
 
 const AboutMe = ({ data }) => {
-    
+
     const ABOUT_ME_DESC = data.wpPage.aboutMeFields.description.replaceAll(/<\/?p>/ig, '\n');
-    
+
     const [imageLoaded, setImageLoaded] = useState(false);
 
     const titleRef = useRef();
@@ -40,45 +40,48 @@ const AboutMe = ({ data }) => {
     }, []);
 
     return (
-        imageLoaded &&
         <Layout>
             <Seo
                 title="About Me"
                 description={ABOUT_ME_DESC}
             />
-            <section className="about-me-section">
-                <CSSTransition in={titleInScreen[0]} timeout={0}>
-                    <h1 ref={titleRef}>About Me</h1>
-                </CSSTransition>
+            {imageLoaded &&
+                <section className="about-me-section">
+                    <CSSTransition in={titleInScreen[0]} timeout={0}>
+                        <h1 ref={titleRef}>About Me</h1>
+                    </CSSTransition>
 
-                <CSSTransition in={descInScreen[0]} timeout={0}>
-                    <div ref={descRef} className="description">
-                        {parse(data.wpPage.aboutMeFields.description)}
-                    </div>
-                </CSSTransition>
+                    <CSSTransition in={descInScreen[0]} timeout={0}>
+                        <div ref={descRef} className="description">
+                            {parse(data.wpPage.aboutMeFields.description)}
+                        </div>
+                    </CSSTransition>
 
-                <CSSTransition in={imgInScreen[0]} timeout={0}>
-                    <img 
-                        className="profile-pic"
-                        src={data.wpPage.aboutMeFields.image.sourceUrl} 
-                        alt={data.wpPage.aboutMeFields.image.altText} 
-                        ref={imgRef}
-                    />
-                </CSSTransition>
-                
-                { (<div className="skills-container">
+                    <CSSTransition in={imgInScreen[0]} timeout={0}>
+                        <img
+                            className="profile-pic"
+                            src={data.wpPage.aboutMeFields.image.sourceUrl}
+                            srcSet={data.wpPage.aboutMeFields.image.srcSet}
+                            sizes={data.wpPage.aboutMeFields.image.sizes}
+                            alt={data.wpPage.aboutMeFields.image.altText}
+                            ref={imgRef}
+                        />
+                    </CSSTransition>
+
+                    {(<div className="skills-container">
                         <CSSTransition in={skillTitleInScreen[0]} timeout={0}>
                             <h2 ref={skillTitleRef}>Skills</h2>
                         </CSSTransition>
                         {data.wpPage.aboutMeFields.skills.map((skill, i) => (
                             <CSSTransition in={skillsInScreen[i]} timeout={0} key={skill}>
-                                <TechIcons ref={skillRefs.current[i]} icon={skill} /> 
+                                <TechIcons ref={skillRefs.current[i]} icon={skill} />
                             </CSSTransition>
                         ))}
                     </div>)
-                }
-                
-            </section>
+                    }
+
+                </section>
+            }
         </Layout>
     )
 }
