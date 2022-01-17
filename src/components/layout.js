0abae01681cef8448868116a9ce66ccc6d88c1a1
Dom-Marra <dominicmarra321@gmail.com>
2022-01-17 12:54:50 -0800

    remove unused code

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,7 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 import Navbar from "./navbar"
 
 const Layout = ({ children }) => {
-  const {
-  } = useStaticQuery(graphql`
-    query LayoutQuery {
-      wp {
-        generalSettings {
-          title
-          description
-        }
-      }
-    }
-  `)
 
   return (
     <>
